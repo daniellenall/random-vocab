ce80e8d345f367f92f5983269e8a499eee747bcc
Danielle <daniellernall@gmail.com>
2020-05-22 20:44:49 +0100

    Created flashcard component

---
+++ components/flashcard.js
@@ -0,0 +1,12 @@
+import styles from './flashcard.module.scss'
+
+
+function FlashCard({ children }) {
+    return (
+        <div className="card">
+            <h2>Apple</h2>
+            <h3>Ringo</h3>
+        </div>)
+}
+
+export default FlashCard
---
+++ pages/cards.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import FlashCard from '../components/flashcard'
+import Layout from '../components/layout'
+
+export default function Home() {
+
+
+    return (
+        <Layout>
+
+            <FlashCard>
+
+            </FlashCard>
+            
+        </Layout>
+
+    )
+}
